13d6a3872e1e311db1b3472857e57a1814edf36a
Michał Chmielewski <michal.ch97@gmail.com>
2018-03-15 21:33:40 +0100

    Zadanie 3 - brak testów

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(final String[] args) {
         SudokuBoard sudokuBoard = new SudokuBoard();
-        sudokuBoard.fillBoard();
+        SudokuSolver sudokuSolver = new BacktrackingSudokuSolver();
+        sudokuSolver.solve(sudokuBoard);
+        boolean boardState = sudokuBoard.checkBoard();
+        if (boardState) {
+            System.out.println("Tablica poprawna!");
+        } else {
+            System.out.println("Tablica zawiera bledy!");
+        }
         sudokuBoard.draw();
     }
 }
--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -1,64 +1,30 @@
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
-
-// https://www.codeproject.com/Articles/23206/Sudoku-Algorithm-Generates-a-Valid-Sudoku-in
 public class SudokuBoard {
-    private Field[] board;
-    private List<List<Integer>> availableNumbersList;
-    private int counter;
-    Random rand;
 
-    public void fillBoard() {
-        board = new Field[81];
-        availableNumbersList = new LinkedList<List<Integer>>();
-        counter = 0;
-        rand = new Random();
-        for (int i = 1; i <= 81; i++) {
-            List<Integer> availableNumbers = new LinkedList<Integer>();
-            for (int j = 1; j <= 9; j++) {
-                availableNumbers.add(j);
-            }
-            availableNumbersList.add(availableNumbers);
-        }
-        while (counter != 81) {
-            if (!availableNumbersList.get(counter).isEmpty()) {
-                int k;
-                if (availableNumbersList.get(counter).size() == 1) {
-                    k = 0;
-                } else {
-                    k = rand.nextInt(availableNumbersList.get(counter).size() - 1);
-                }
-                int z = availableNumbersList.get(counter).get(k);
-                Field tempField = new Field(counter, z);
-                if (!conflict(board, tempField)) {
-                    board[counter] = tempField;
-                    availableNumbersList.get(counter).remove(k);
-                    counter++;
-                } else {
-                    availableNumbersList.get(counter).remove(k);
-                }
-            } else {
-                for (int j = 1; j <= 9; j++) {
-                    availableNumbersList.get(counter).add(j);
-                }
-                board[counter - 1] = null;
-                counter--;
-            }
-        }
-    }
+    private Field[] board = new Field[81];
 
-    private boolean conflict(final Field[] allFields, final Field field) {
-        for (Field f : allFields) {
-            if (f != null) {
-                if ((f.getxAxis() != 0 && (f.getxAxis() == field.getxAxis())) || (f.getyAxis() != 0 && (f.getyAxis() == field.getyAxis())) || (f.getSector() != 0 && (f.getSector() == field.getSector()))) {
-                    if (f.getValue() == field.getValue()) {
-                        return true;
+    public boolean checkBoard() {
+        for (Field f1 : board) {
+            for (Field f2 : board) {
+                if (f1 != f2) {
+                    if ((f1.getxAxis() == f2.getxAxis()) || (f1.getyAxis() == f2.getyAxis()) || (f1.getSector() == f2.getSector())) {
+                        if (f1.getValue() == f2.getValue()) {
+                            return false;
+                        }
                     }
                 }
             }
         }
-        return false;
+        return true;
+    }
+
+    public int get(int x, int y) {
+        int index = (x - 1) + (y - 1) * 9;
+        return board[index].getValue();
+    }
+
+    public void set(int x, int y, int value) {
+        int index = (x - 1) + (y - 1) * 9;
+        board[index] = new Field(index, value);
     }
 
     public void draw() {
@@ -85,7 +51,16 @@ public class SudokuBoard {
         System.out.println("-------------------------");
     }
 
-    public Field[] getBoard() {
-        return board;
+    public void setNull(int i) {
+        board[i] = null;
+    }
+
+    public boolean checkNull(int x, int y) {
+        int index = (x - 1) + (y - 1) * 9;
+        if (board[index] != null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
