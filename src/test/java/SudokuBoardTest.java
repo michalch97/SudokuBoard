620c2daae573032afdd457cb1ecc9d58967e2874
Bartłomiej Ciesielski <bartekc8@op.pl>
2018-03-17 18:37:58 +0100

    Dodanie testu klasy Field i dodatkowego testu dla SudokuBoard

--- src/test/java/SudokuBoardTest.java
+++ src/test/java/SudokuBoardTest.java
@@ -4,11 +4,23 @@ import org.junit.Test;
 public class SudokuBoardTest {
     
     @Test
-    public void testCheckFunction() {
+    public void checkIfCheckFunctionWorksOnCorrectBoard() {
         SudokuBoard sudokuBoard = new SudokuBoard();
         SudokuSolver sudokuSolver = new BacktrackingSudokuSolver();
         sudokuSolver.solve(sudokuBoard);
         
         Assert.assertTrue(sudokuBoard.checkBoard());
+    }
+    
+    @Test
+    public void checkIfCheckFunctionWorksOnIncorrectBoard() {
+        SudokuBoard sudokuBoard = new SudokuBoard();
+        SudokuSolver sudokuSolver = new BacktrackingSudokuSolver();
+        sudokuSolver.solve(sudokuBoard);
+        
+        int existingNumber = sudokuBoard.get(0);
+        sudokuBoard.set(1, existingNumber);
+        
+        Assert.assertFalse(sudokuBoard.checkBoard());
     }
 }
